1b596f92249cb876facf676fe8f3fff929add395
dnunezmo <38168642+dnunezmo@users.noreply.github.com>
2019-03-08 10:29:12 +0200

    Update Footer.vue

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Kubernetting in Bucharest is Cool...!!! (but sometimes with errors)
+        Azure Global Blackbelt Team
       </div>
       <div class="col-lg-6">
       </div>
